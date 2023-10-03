2867120b60f648004f9f91599f00386282ea1230
Mariam Aly <mariam.aly@bdaiat.com>
2023-10-03 10:55:19 +0300

    responsive

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -4,11 +4,14 @@ import Image from "next/image";
 import Link from "next/link";
 import logo from "../components/Images/Logo.png"
 import { usePathname } from "next/navigation"
+import { useState } from "react";
 export const Navbar = () => {
+	const [toggle,setToggle]=useState(false)
 	const pathname = usePathname();
 	return (
 	<>
 	<div className="navigatioTop">
+	
 	تقديم الرؤى لتعزيز الصحة المالية لأعمالك.
 	</div>
 	<div  className="navSticky" >
@@ -33,7 +36,7 @@ src={logo} alt="المركز الإستشاري المصري"/>
 		
 		</div>	</a>
 
-			<div>
+			<div className="links">
 
 			
 
@@ -63,6 +66,103 @@ src={logo} alt="المركز الإستشاري المصري"/>
 
 			</div>
 		</nav>
+
+
+		<div id="navbarMobile" >
+		<div  style={{display:"flex",alignItems:"center"}}>
+
+
+
+		
+<Image 
+
+width={81}
+height={77}
+src={logo} alt="المركز الإستشاري المصري"/>
+
+<div className="compLogo">
+	<p className="compName">المركز الإستشاري المصري</p>
+	<p className="compDet">للمحاسبة والمراجعة والخدمات الضريبية</p>
+	</div>
+
+</div>
+        <button onClick={()=>setToggle(true)}> 
+		
+		<svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 24" fill="none">
+<path d="M3 12H15M3 6H21M3 18H21" stroke="#667085" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+</svg>		
+		
+		</button>
+   
+	
+<div  className={toggle?'sideNav ':'sideNav navClosed'}>
+
+</div>
+<button className={toggle?'closeNav ':'closeNav closeNavHidden'} onClick={()=>setToggle(false)} >
+<svg xmlns="http://www.w3.org/2000/svg" width="34" height="34" viewBox="0 0 24 24" fill="none">
+<path d="M18 6L6 18M6 6L18 18" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+</svg>
+</button>
+
+<div className={toggle?'mobileLinks':'mobileLinksClose mobileLinks'}>
+<div  style={{display:"flex",alignItems:"center"}}>
+
+
+
+		
+<Image 
+
+width={81}
+height={77}
+src={logo} alt="المركز الإستشاري المصري"/>
+
+<div className="compLogo">
+	<p className="compName">المركز الإستشاري المصري</p>
+	<p className="compDet">للمحاسبة والمراجعة والخدمات الضريبية</p>
+	</div>
+
+</div>
+{/*
+<div id="srchInput" className='navSearch'>
+        <FiSearch size={20}  color='#667085'/>
+
+<input  placeholder={t("dashboard.search")}/>
+
+  </div>*/}
+        <ul id="navList">
+			<li>
+
+		
+		<Link  onClick={()=>setToggle(false)} className={pathname == "/contact" ? "active" : ""} href="/contact" style={{marginLeft:"0"}} > 
+			تواصل معنا</Link>
+			</li>
+			<li>
+			<Link onClick={()=>setToggle(false)}  className={pathname == "/services" ? "active" : ""} href="/services"> 
+			خدماتنا
+			</Link></li>
+			<li>
+			<Link  onClick={()=>setToggle(false)}  className={pathname == "/about" ? "active" : ""} href="/about">   من نحن </Link>
+
+</li>
+<li>
+			<Link onClick={()=>setToggle(false)}  className={pathname == "/" ? "active" : ""} href="/"> 
+			الرئيسية
+			</Link>
+			</li>
+  
+</ul>
+
+
+
+	<div className="socials">
+	
+				<p>تواصل معنا من خلال</p>
+				<a href="/">201029339203+</a>
+
+			</div></div>
+    
+
+</div>
 			</div>
 
 		</>
